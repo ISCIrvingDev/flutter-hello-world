dc7f3ceb6a2e7f35048f452d3e08559e0e354f8f
Irving Salazar <ISCIrvingDev@gmail.com>
2025-03-04 01:41:36 -0600

    Add AppBar & Container

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Hello Flutter'),
     );
   }
 }
@@ -28,37 +28,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
+        // backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        backgroundColor: Colors.blue[900],
         title: Text(widget.title),
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        // color: Colors.blue[200],
+        decoration: BoxDecoration(
+          color: Colors.blue[100],
+          // borderRadius: BorderRadius.circular(16),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        padding: EdgeInsets.all(20),
+        child: const Text('Hello Flutter'),
+        // margin: EdgeInsets.only(top: 20),
       ),
     );
   }
